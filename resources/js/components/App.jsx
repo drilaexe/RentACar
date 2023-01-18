12035d67e5830cc174c770d9be93ddf66aeb6678
Drilon Syla <drilonsyla123@gmail.com>
2023-01-18 02:00:18 +0100

    addedd jsx

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function Example() {
+import Table from './Cars/Table';
+function App() {
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">Lati Component</div>
 
                         <div className="card-body">I'm an example component!</div>
                     </div>
                 </div>
+                <Table/>
             </div>
         </div>
     );
 }
 
-export default Example;
-
+export default App;
